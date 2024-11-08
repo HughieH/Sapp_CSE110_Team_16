3e6313f0b01f1bc1e25b43d7f7dfe8860f8af52e
jiho-98 <129816454+jiho-98@users.noreply.github.com>
2024-11-07 17:07:42 -0800

    basic UI for the login page

---
+++ README.old.md
@@ -0,0 +1,1 @@
+## CSE 110 Team 16 Group Project - Sapp, a web app that makes studying better!
---
+++ src/login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import logo from './images/logo.png';
+import image from './images/image.png';
+import './login.css';
+
+function Login() {
+    return (
+        <div className="container">
+            <div className="left-panel">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+            
+            <div className="right-panel">
+                <div className="login-content">
+                    <div className="login-container">
+                        <h1>Log in</h1>
+                        <form>
+                            <div>
+                                <input type="email" placeholder="name@example.com" />
+                            </div>
+                            <div>
+                                <input type="password" placeholder="Password" />
+                            </div>
+                            <button type="submit">Log In</button>
+                        </form>
+                    </div>
+                    <img src={image} alt="Additional" className="additional-image" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
