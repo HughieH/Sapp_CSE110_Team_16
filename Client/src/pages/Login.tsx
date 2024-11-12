1eaece0acbf0ec3e3e15d8a8d1783a65baca3583
jiho-98 <129816454+jiho-98@users.noreply.github.com>
2024-11-11 17:53:21 -0800

    Add TypeScript login page component

--- Client/src/pages/Login.tsx
+++ Client/src/pages/Login.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import logo from '../images/logo.png';
+import image from '../images/image.png';
+import './login.css';
 
-const Login = () => {
-  return (
-    <div className="flex justify-center items-center h-screen bg-yellow-500 text-white">
-      <h1 className="text-4xl font-bold">Login Page</h1>
-    </div>
-  );
+const Login: React.FC = () => {
+    return (
+        <div className="container">
+            <div className="left-panel">
+                <img src={logo} alt="Logo" className="logo" />
+            </div>
+            
+            <div className="right-panel">
+                <div className="login-content">
+                    <div className="login-container">
+                        <h1>Log in</h1>
+                        <form>
+                            <div>
+                                <input type="email" placeholder="name@example.com" />
+                            </div>
+                            <div>
+                                <input type="password" placeholder="Password" />
+                            </div>
+                            <button type="submit">Log In</button>
+                        </form>
+                    </div>
+                    <img src={image} alt="Additional" className="additional-image" />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Login;
